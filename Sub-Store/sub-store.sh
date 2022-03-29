73f4f9f922834c324a896e670518e9da2b7cff7a
ShellManager <374779789@qq.com>
2022-03-29 13:41:31 +0800

    更新

--- Sub-Store/sub-store.sh
+++ Sub-Store/sub-store.sh
@@ -6,20 +6,23 @@ backend="$rootPath/backend"
 web="$rootPath/web"
 nginx="$rootPath/nginx"
 
+cd /git/backend
 echo "初始化配置。。。"
 if [ ! -f "$web/package.js" ]; then    
     cp -r /git/. /Sub-Store
+    cd "$web" && cnpm install 
+    cd "$backend" && cnpm install 
 fi
 
 echo "删除自带后端地址，追加配置环境变量配置的后端地址。。。"
 sed -i "/BACKEND_BASE\|DEBUG\|DOMIAN/d" "$web/src/config.js"
 echo "export const BACKEND_BASE = '${DOMAIN}';" >>"$web/src/config.js"
-
+ 
 echo "判断是否存在前端静态资源。。。"
 if [ ! -d "$web/dist" ]; then
     cd "$web"
     echo "执行编译前端静态资源。。。"    
-    node build
+    npm run build
     cp -rf  "$web/dist" /var/www/sub-store
     echo "结束编译"
 fi
@@ -31,4 +34,4 @@ nginx -s reload
 
 echo "启动后端服务。。。"
 cd "$backend"
-pm2 start sub-store.js >>sub-store.log 2>&1 &
+pm2 start sub-store.js
