9c9869c4f0461e6383a3a1c99ca4851bc8cc5fa2
ShellManager <374779789@qq.com>
2022-03-31 11:15:05 +0800

    shell

--- Sub-Store/sub-store.sh
+++ Sub-Store/sub-store.sh
@@ -6,21 +6,21 @@ backend="$rootPath/backend"
 web="$rootPath/web"
 nginx="$rootPath/nginx"
 
-
-echo -e "======================== 1、启动 UI 界面 ========================\n"
+echo -e "======================== 1. 启动nginx ========================\n"
 mkdir -p /etc/nginx/conf.d
+mkdir -p /run/nginx
 cp -r /Sub-Store/nginx/. /etc/nginx/conf.d
 echo -e "生成 nginx 配置文件\n"
 envsubst '${ALLOW_IP}' < /etc/nginx/conf.d/front.template > /etc/nginx/conf.d/front.conf && nginx -g 'daemon off;'
-
-nginx -c /etc/nginx/nginx.conf 
-nginx -s reload
+nginx -s reload 2>/dev/null || nginx -c /etc/nginx/nginx.conf
+echo -e "==============================================================\n"
 
 echo -e "======================== 2、启动后端接口 ========================\n"
 cd "$backend"
-pm2 start sub-store.js
+pm2 start sub-store.js -n public --source-map-support --time
+echo -e "==============================================================\n"
 
-echo -e "======================== 3、验证 UI 界面 ========================\n"
+echo -e "======================== 3、启动 Sub-Store 界面 ========================\n"
 if [ ! -f "$web/dist/index.html" ]; then
     echo -e "删除自带后端地址，追加配置环境变量配置的后端地址\n"
     sed -i "/BACKEND_BASE\|DEBUG\|DOMIAN/d" "$web/src/config.js"
@@ -28,9 +28,11 @@ if [ ! -f "$web/dist/index.html" ]; then
     cd "$web"
     echo -e "执行编译前端静态资源\n"    
     npm run build
-    echo -e "结束编译\n"
+    echo -e "结束编译，UI 界面已生成\n"
 else
-    echo -e "验证结束\n"     
+    echo -e "验证结束，UI 界面已生成\n"     
 fi
+echo -e "==============================================================\n"
 
 pm2 log sub-store
+exec "$@"
